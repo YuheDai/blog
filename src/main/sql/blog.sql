2b2df217ffb34cbb08331594b15b502477db8ad6
daiyuhe <dai.zycc@qq.com>
2018-12-25 13:15:22 +0800

    添加注释

--- src/main/sql/blog.sql
+++ src/main/sql/blog.sql
@@ -6,7 +6,7 @@ CREATE database blog;
 use blog;
 
 --创建表
---创建文章表
+--创建用户表
 CREATE TABLE blog_user(
 object_id varchar(19) not null COMMENT'主键',
 name varchar(255) not null COMMENT'用户昵称',
@@ -18,10 +18,11 @@ article_count int not null default 0 COMMENT'用户文章计数',
 primary key(object_id)
 )ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 collate utf8mb4_general_ci COMMENT='博客用户表';
 
+--创建文章表
 CREATE table blog_article(
 object_id varchar(19) not null COMMENT'主键',
 title varchar(255) not null COMMENT'文章标题',
-abstract text not null COMMENT'文章摘要',
+abstracts text not null COMMENT'文章摘要',
 content mediumtext not null COMMENT'文章内容',
 tags varchar(255) COMMENT '文章标签,使用逗号分隔',
 author_id varchar(19) not null COMMENT'作者id',
@@ -37,10 +38,11 @@ view_password varchar(255) COMMENT'文章浏览密码',
 primary key(object_id)
 )ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 collate utf8mb4_general_ci COMMENT='博客文章表';
 
+--创建评论表
 create table blog_comment(
 object_id varchar(19) not null COMMENT'主键',
 content text not null COMMENT'评论内容',
-creat_time timestamp not null default current_timestamp COMMENT'评论创建时间戳',
+create_time timestamp not null default current_timestamp COMMENT'评论创建时间戳',
 name varchar(50) not null COMMENT'评论人昵称',
 article_id varchar(19) not null COMMENT'评论文章的id',
 sharp_url varchar(255) not null COMMENT'评论访问路径(#锚点)',
@@ -50,6 +52,7 @@ original_name varchar(50) COMMENT'父评论昵称',
 primary key(object_id)
 )ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 collate utf8mb4_general_ci COMMENT='博客评论表';
 
+--创建标签表
 create table blog_tag(
 object_id varchar(19) not null COMMENT'主键',
 title varchar(255) not null COMMENT'标签标题',
@@ -57,6 +60,7 @@ published_article int not null COMMENT'标签关联的已发布文章数量',
 primary key(object_id)
 )ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 collate utf8mb4_general_ci COMMENT='博客标签表';
 
+--创建分类表
 create table blog_category(
 object_id varchar(19) not null COMMENT'主键',
 title varchar(255) not null COMMENT'分类标题',
@@ -65,6 +69,7 @@ tag_count int not null COMMENT'分类下聚合的标签数',
 primary key(object_id)
 )ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 collate utf8mb4_general_ci COMMENT='博客分类表';
 
+--创建存档日期表
 create table blog_archive(
 object_id varchar(19) not null COMMENT'主键',
 article_count int not null COMMENT'存档日期文章计数,即某月文章总数',
@@ -72,6 +77,7 @@ archive_date timestamp not null COMMENT'存档日期时间',
 primary key(object_id)
 )ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 collate utf8mb4_general_ci COMMENT='博客存档日期表';
 
+--创建配置表
 create table blog_option(
 object_id varchar(19) not null COMMENT'主键(配置项键名)',
 option_value text not null COMMENT'配置项值',
@@ -79,6 +85,7 @@ option_category varchar(19) not null COMMENT'配置项分类',
 primary key(object_id)
 )ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 collate utf8mb4_general_ci COMMENT='博客配置表';
 
+--创建文章-存档日期关联表
 create table blog_article_archive(
 object_id varchar(19) not null COMMENT'主键',
 article_id varchar(19) not null COMMENT'文章的主键',
@@ -86,6 +93,7 @@ archive_id varchar(19) not null COMMENT'存档的主键',
 primary key(object_id)
 )ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 collate utf8mb4_general_ci COMMENT='文章-存档关联表';
 
+--创建文章-标签关联表
 create table blog_article_tag(
 object_id varchar(19) not null COMMENT'主键',
 article_id varchar(19) not null COMMENT'文章的主键',
@@ -93,6 +101,7 @@ tag_id varchar(19) not null COMMENT'标签的主键',
 primary key(object_id)
 )ENGINE=InnoDB DEFAULT CHARACTER SET utf8mb4 collate utf8mb4_general_ci COMMENT='文章-标签关联表';
 
+--创建分类-标签关联表
 create table blog_category_tag(
 object_id varchar(19) not null COMMENT'主键',
 category_id varchar(19) not null COMMENT'分类的主键',
